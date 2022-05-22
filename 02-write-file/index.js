a9c8939feffb286b8da18952d780a5b6b54b9762
Sergey Kornienko <madkorney@gmail.com>
2022-05-22 19:17:50 +0300

    feat: implemented task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, stderr } = require('process');
+const fileName = 'text.txt';
+const filePathName = path.join(__dirname, fileName);
+const writeStream = fs.createWriteStream(filePathName, 'utf-8');
+
+process.on('SIGINT', () => {
+  stdout.write(`input completed. file is written at : ${filePathName}\n`);
+  writeStream.close();
+  process.exit();
+});
+
+stdin.on('data', data => {
+  if (data.toString().substring(0, 4) === 'exit') {
+    stdout.write(`input completed. file is written at : ${filePathName}\n`);
+    writeStream.close();
+    process.exit();
+  }
+  writeStream.write(data);
+});
+
+writeStream.on('error', error => stderr.write(`Error: ${error.message}`));
+stdin.on('error', error => stderr.write(`Error: ${error.message}`));
+
+console.log('Please type some text to be written to file. For exit type "exit" or press Ctrl-C.');
+
